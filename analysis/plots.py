8d45fdc5db66e9c0eb5115f539ec81a72e967841
Daniel Kiely <dmk118@georgetown.edu>
2025-12-18 01:40:41 -0500

    local

--- analysis/plots.py
+++ analysis/plots.py
@@ -1,24 +1,51 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data
-data = pd.read_csv('analysis/data/warmed_up_training.csv')
+data = pd.read_csv('analysis/data/joint_training.csv')
 
-# Create the plot
-plt.figure(figsize=(10, 5))
-plt.plot(data['Step'], data['Attacker Reward'], label='Attacker')
-plt.plot(data['Step'], data['Defender Reward'], label='Defender')
+plt.figure(figsize=(3.5, 2.5))
 
-# Add vertical lines at steps 20, 40, 60
-for x in [0, 20, 40, 60, 80]:
-    plt.axvline(x=x, color='gray', linestyle=':', alpha=0.5)
 
-# Add labels and legend
-plt.xlabel('Step')
-plt.ylabel('Reward')
-plt.legend()
+plt.plot(
+    data['Step'],
+    data['Defender Reward'],
+    label='Defender',
+    linewidth=2.0
+)
 
-# Save and show
+plt.plot(
+    data['Step'],
+    data['Attacker Reward'],
+    label='Attacker',
+    linewidth=2.0
+)
+
+
+# Optional: keep only key vertical markers
+for x in [20, 40, 60]:
+    plt.axvline(x=x, linestyle=':', alpha=0.3)
+
+plt.xlabel('Step', fontsize=9)
+plt.ylabel('Reward', fontsize=9)
+
+plt.legend(
+    fontsize=8,
+    frameon=False,
+    loc='center right',
+    bbox_to_anchor=(1.0, 0.65)
+)
+
+plt.tick_params(axis='both', labelsize=8)
+plt.grid(alpha=0.2)
+
+plt.ylim(-.05, 1.05)
+
 plt.tight_layout()
-plt.savefig('analysis/plots/warmed_up_training_rewards.png')
+plt.savefig(
+    'analysis/plots/joint_training_rewards.png',
+    dpi=300,
+    bbox_inches='tight'
+)
+
+
 plt.show()
